38521b20ac13cb5481b5a1c336f264eca2bc5cfe
jns655737-hub <jns655737@gmail.com>
2026-03-03 15:45:31 +0530

    Condition statement

---
+++ W3/D2/p4.js
@@ -0,0 +1,35 @@
+// // Conditional Statement
+// let age=31;
+// if(age>=18){
+//     console.log("adult")
+// }
+// else if(age>30){
+//     console.log("Old people")
+// }
+// else{
+//     console.log("child")
+// }
+
+// // Switch Satement
+// console.log("Conditional Statement");
+// let day="c";
+// switch(day){
+//     case "c":
+//         console.log("start of the week");
+//         break;
+
+//     case "wednesday":
+//         console.log("Mid of the week");
+//          break;
+//     case "friday":
+//         console.log("end of the week");
+//         break;
+
+//         default:
+//             console.log("Some day in the week");
+//             break;
+// }
+// Type Conversion
+let n=Number("ABC");
+console.log("Type of n:",typeof n," ", "isNaN :",isNaN(n));
+console.log(n);
