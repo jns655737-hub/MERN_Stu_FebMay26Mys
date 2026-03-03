fedd72a0dd6ea2e1c916c3c456677f4df2b69005
jns655737-hub <jns655737@gmail.com>
2026-03-03 15:44:45 +0530

    Comparison operators

---
+++ W3/D2/p2.js
@@ -0,0 +1,12 @@
+// Comparison operators
+
+console.log("Comparison operators");
+let x=10,y='10';
+console.log("x==y:",x==y);//Loose equality
+console.log("x===y:",x===y);//Strict equality
+console.log("x!=y:",x!=y);//Loose inequality
+console.log("x!==y:",x!==y);//strict inequality
+console.log("x>3:",x>3);
+console.log("x<3:",x<3);
+console.log("x>=3:",x>=3);
+console.log("x<=3:",x<=3);
