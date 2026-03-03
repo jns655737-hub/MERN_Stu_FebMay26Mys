68cd108329dc544b6dad810eb3b18b04fa0a7e3c
jns655737-hub <jns655737@gmail.com>
2026-03-03 15:44:22 +0530

    Arithmetic operation

---
+++ W3/D2/p1.js
@@ -0,0 +1,15 @@
+//Arthematic expression
+console.log("Arithmetic operations");
+let a=10;
+let b=5;
+let c=a+b;
+console.log("a+b",a+b);
+console.log("a-b",a-b);
+console.log("a*b",a*b);
+console.log("a/b",a/b);
+console.log("a%b",a%b);//Modulus
+console.log("a**b",a**b);//Exponential
+console.log("a++",a++);//Post increment
+console.log("++a",++a);//Pre increment
+console.log("a--",a--);//Post decrement
+console.log("--a",--a);//Pre decrement
