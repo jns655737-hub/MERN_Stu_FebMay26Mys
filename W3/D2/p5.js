91c2f84d99c0ec01ba3165c14b92ffc4e0f06859
jns655737-hub <jns655737@gmail.com>
2026-03-03 15:45:52 +0530

    Function declaration

---
+++ W3/D2/p5.js
@@ -0,0 +1,6 @@
+// Function declaration
+greet("Jeevan") //hoisting is also supported for functions
+function greet(name){           //declaration
+   console.log("Hello",name)    //defination
+}
+ //greet("Jeevan")                //calling
