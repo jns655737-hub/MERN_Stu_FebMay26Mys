9383a7f4ead42b9a0e6398e5cdbe85ec52020229
jns655737-hub <jns655737@gmail.com>
2026-03-03 15:46:15 +0530

    Function expression

---
+++ W3/D2/p6.js
@@ -0,0 +1,8 @@
+//Function expression
+const greet=function greet(name){           //declaration
+   console.log("Hello",name)    //defination
+}
+ greet("Jeevan")            //calling
+
+//  console.log(greet);
+ 
