65c4b96b6023cdb680d05a52b5c09336ad1689e6
jns655737-hub <jns655737@gmail.com>
2026-03-04 14:27:52 +0530

    // for loop

---
+++ W3/D3/p1.js
@@ -0,0 +1,18 @@
+// for loop
+console.log("for loop examples");
+
+for(let i=0;i<5;i++){
+    console.log("i:",i); //forward loop within for loop   
+}
+
+
+for(let i=5;i>0;i--){
+    console.log("i1:",i); //backward loop within for loop   
+}
+
+for(let i=1;i<=3;i++){
+    for(let j=1;j<=2;j++){
+        console.log(`i=${i},j=${j}`);
+        
+    }
+}
