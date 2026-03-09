ae3512489556d19b15b2a13675039424efdc185e
jns655737-hub <jns655737@gmail.com>
2026-03-09 23:09:47 +0530

    //Reduce function

---
+++ W3/D4/p5.js
@@ -0,0 +1,24 @@
+//Reduce function 
+//sum
+let nums=[5,10,15];
+
+let total=nums.reduce((intermediateSum,current)=>
+intermediateSum+current,0);
+console.log(total);
+
+
+// Average
+// let nums1=[5,10,15];
+
+// let Average=nums1.reduce((intermediateSum,current)=>intermediateSum+current/nums1.length,0);
+// console.log(Average);
+
+//reduce to object count by categeory
+let items=["pen","pencil","eraser","pen"];
+
+let count=items.reduce((intermediatevalue,items)=>{
+    intermediatevalue[items]=
+    (intermediatevalue[items]||0)+1;
+    return intermediatevalue;
+},{});
+console.log("items count:",count);
