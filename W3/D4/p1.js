d09d754490d1b06bb8c42d29464e00b50072058c
jns655737-hub <jns655737@gmail.com>
2026-03-09 23:07:45 +0530

    //Basics array concept

--- W3/D4/p1.js
+++ W3/D4/p1.js
@@ -1,17 +1,17 @@
 //Basics array concept
 console.log("Arrrays");
 //creating arrays
-let emptyArray=[];
-let numArray=[1,2,3,4,5];
-let mixedArray=["Hello",18,'J',true,{name:"Jeevan"},null,[6,9]];
-console.log(emptyArray);
-console.log(numArray);
-console.log(mixedArray);
+// let emptyArray=[];
+// let numArray=[1,2,3,4,5];
+// let mixedArray=["Hello",18,'J',true,{name:"Jeevan"},null,[6,9]];
+// console.log(emptyArray);
+// console.log(numArray);
+// console.log(mixedArray);
 
 //creation of the arrays using constructors
 let fruits=new Array("Apple","Mango","Jackfruit");
-console.log(fruits);
-console.log("Is fruits an array?",Array.isArray(fruits));
+// console.log(fruits);
+// console.log("Is fruits an array?",Array.isArray(fruits));
 
 //Push function to add the value to the array
 fruits.push("graphs")
@@ -28,6 +28,7 @@ console.log(fruits);
 //shift is to remove value from begining
 fruits.shift();
 console.log(fruits);
+
 
 
 
