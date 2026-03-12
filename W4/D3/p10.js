49010008100ba4d762692b50499bcdaa2d400877
jns655737-hub <jns655737@gmail.com>
2026-03-12 11:53:17 +0530

    Async fetch firstthre

---
+++ W4/D3/p10.js
@@ -0,0 +1,20 @@
+const asyncFetchBtn=document.getElementById("asyncFetchBtn");
+const output=document.getElementById("output");
+
+asyncFetchBtn.addEventListener("click",
+    async function(){
+        try{
+            const response=await
+            fetch("https://jsonplaceholder.typicode.com/users")
+            if(!response.ok)throw new Error("HTTP Error:"
+                +response.status);
+                const users=await response.json();
+                const firstThree=users.slice(0,3);
+                output.textContent=JSON.stringify(firstThree,null,2);
+                
+                // const
+        }
+        catch(error){
+           output.textContent="Error:",+error.message;
+        }
+    })
