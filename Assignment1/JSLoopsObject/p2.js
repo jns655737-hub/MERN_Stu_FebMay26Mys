1ee73f1deb7c284501ab6bbb323775e47aea2a11
jns655737-hub <jns655737@gmail.com>
2026-03-13 09:56:08 +0530

    p2.js.loops & objects

---
+++ Assignment1/JSLoopsObject/p2.js
@@ -0,0 +1,43 @@
+function invoice(gstRate = 0.18, ...items) {
+
+    let subtotal = 0;
+
+    for (let i = 0; i < items.length; i++) {
+
+        let item = items[i];
+
+        
+        if (item.name === "STOP") {
+            break;
+        }
+
+        
+        if (!item.price || !item.qty || item.price <= 0 || item.qty <= 0) {
+            continue;
+        }
+
+        subtotal += item.price * item.qty;
+    }
+
+    let gst = subtotal * gstRate;
+    let total = subtotal + gst;
+
+    return {
+        subtotal: subtotal,
+        gst: gst,
+        total: total
+    };
+}
+
+
+
+let result = invoice(
+    0.18,
+    { name: "Pen", price: 10, qty: 3 },
+    { name: "Book", price: 50, qty: 2 },
+    { name: "Pencil", price: -5, qty: 5 }, // ignored
+    { name: "STOP", price: 100, qty: 1 },
+    { name: "Bag", price: 200, qty: 1 } // not processed
+);
+
+console.log(result);
