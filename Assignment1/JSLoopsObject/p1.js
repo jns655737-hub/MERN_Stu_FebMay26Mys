eed69f8310d10e23bf380a6662c0d53bb996614e
jns655737-hub <jns655737@gmail.com>
2026-03-13 09:55:00 +0530

    js.loops & objects

---
+++ Assignment1/JSLoopsObject/p1.js
@@ -0,0 +1,38 @@
+function tagPassword(password) {
+
+    if (typeof password !== "string") {
+        return "INVALID";
+    }
+
+    let hasLetter = false;
+    let hasNumber = false;
+
+    for (let i = 0; i < password.length; i++) {
+        let ch = password[i];
+
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            hasLetter = true;
+        }
+
+        if (ch >= '0' && ch <= '9') {
+            hasNumber = true;
+        }
+    }
+
+    if (password.length >= 12 && hasLetter && hasNumber) {
+        return "STRONG";
+    }
+
+    if (password.length >= 8 && hasLetter && hasNumber) {
+        return "MEDIUM";
+    }
+
+    if (password.length < 8) {
+        return "WEAK";
+    }
+}
+
+console.log(tagPassword("Jeevan"));        // WEAK
+// console.log(tagPassword("Jeevanns1415"));      // MEDIUM
+// console.log(tagPassword("Jeevnns0181415Strongpassword"));  // STRONG
+// console.log(tagPassword(12345));           // INVALID
