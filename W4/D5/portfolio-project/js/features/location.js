c4da1161259beaf9922ecb2ac79e761e3f1e694e
jns655737-hub <jns655737@gmail.com>
2026-03-13 17:22:42 +0530

    W-4 portfolio-project

---
+++ W4/D5/portfolio-project/js/features/location.js
@@ -0,0 +1,34 @@
+function initLocation(){
+const out=document.getElementById("out");
+
+function explain(code){
+    if(code ===1) return "Permission declares";
+    if(code ===2) return "Position Unavailable";
+    if(code ===3) return "Request Timed Out";
+    return "Unknown Error";
+}
+document.getElementById("locateBtn").addEventListener("click",
+    function(){
+        if(!navigator.geolocation){
+            out.textContent="geolocation is not Supported in this Browser"
+        }
+        out.textContent="Requesting the current location.....";
+        navigator.geolocation.getCurrentPosition(
+        function(position){
+            const data={
+                latitude:position.coords.latitude,
+                longitude:position.coords.longitude,
+                accuracy:position.coords.accuracy
+            };
+            console.log(data);
+            out.textContent=JSON.stringify(data,null,2);
+            
+        },
+        function(error){
+            out.textContent="Error code:"+error.code+explain(error.code)+
+            "Message:"+error.message;
+        },
+        {enableHighAccuracy:true,timeout:10000,maximumAge:0}
+        );
+});
+}
