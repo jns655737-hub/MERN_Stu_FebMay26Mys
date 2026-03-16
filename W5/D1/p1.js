11ad23ed4904b7ee03cdfeff64b8d130f342b900
jns655737-hub <jns655737@gmail.com>
2026-03-16 16:23:47 +0530

    Introduction to node.js

---
+++ W5/D1/p1.js
@@ -0,0 +1,19 @@
+// Introduction to Node.js
+
+// const { use } = require("react");
+
+// const runtimeName="Node.js";
+// console.log("Introduction to Node.js");
+// console.log(`${runtimeName} runs javascript outside browser`);
+
+const commonUser=[
+    "Used for Server side application","automation script can be created"
+];
+
+// console.log(commonUser[0]);
+// console.log(commonUser[1]);
+commonUser.forEach((Use,index) =>{
+      console.log(`${index+1}.${Use}`);
+      
+})
+
