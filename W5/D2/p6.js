e7465194ade5f302f9e72033808a46c66cdd7478
jns655737-hub <jns655737@gmail.com>
2026-03-17 13:30:49 +0530

    //callback nesting

---
+++ W5/D2/p6.js
@@ -0,0 +1,19 @@
+//callback nesting
+console.log("Nested callback loop");
+setTimeout(function(){
+        console.log("Step 1:User loaded");
+
+        setTimeout(function(){
+            console.log("Step 2:Orders loaded");
+
+            setTimeout(function(){
+                console.log("Step 3:Payments loaded");
+                
+                setTimeout(function(){
+                    console.log("Step 4:Shipment loaded.");
+                    console.log("We are in callback hell!!!");
+                    
+                },1000);
+            },800);
+        },600);
+},400);
