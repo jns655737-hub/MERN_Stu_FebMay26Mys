52e18fe4403768d73c6435543141579489ea6e55
jns655737-hub <jns655737@gmail.com>
2026-03-17 14:35:07 +0530

    //callback handling with name functions

---
+++ W5/D2/p7.js
@@ -0,0 +1,35 @@
+//callback handling with name functions
+function loadUser(next){
+   setTimeout(function(){
+    console.log("Step 1:User loaded.");
+    next();
+   },400);
+}
+function loadOrders(next){
+   setTimeout(function(){
+    console.log("Step 2:User Orders.");
+    next();
+   },400);
+}
+function loadPayments(next){
+    setTimeout(function(){
+        console.log("Step 3:load Payments");
+        next();
+    },400);
+}
+function loadShipment(){
+    setTimeout(function(){
+        console.log("Step 4:load Shipment");
+        console.log("same flow but easier to read");
+        
+    },400);
+}
+loadUser(function(){
+    loadOrders(function(){
+        loadPayments(function(){
+            loadShipment()
+        })
+    })
+
+    
+});
