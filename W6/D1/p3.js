24a4bb09598b32e7ccca4397ab8d5c1ecad5d74e
jns655737-hub <jns655737@gmail.com>
2026-03-25 11:53:49 +0530

    Using a custom commonJS module

---
+++ W6/D1/p3.js
@@ -0,0 +1,17 @@
+// // Using a custom commonJS module
+
+
+const { caluclateTax, applyDiscount, formatCurrency } = require("./p2");
+
+const itemPrice = "Laptop";
+const basePrice = 60000;
+
+const discountedPrice = applyDiscount(basePrice, 10);
+const taxAmount = caluclateTax(discountedPrice);
+const finalPrice = discountedPrice + taxAmount;
+
+console.log("Item:", itemPrice);
+console.log("Base price:", formatCurrency(basePrice));
+console.log("Discounted price:", formatCurrency(discountedPrice));
+console.log("GST Tax @ 18%:", formatCurrency(taxAmount));
+console.log("Final price:", formatCurrency(finalPrice));
