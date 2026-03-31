bac285016bf0b7de425c8ca8a1aaba23dffc6f04
jns655737-hub <jns655737@gmail.com>
2026-03-31 16:04:14 +0530

    p6.js

---
+++ W7/D1/p6.js
@@ -0,0 +1,22 @@
+//
+const express=require("express");
+
+const app=express();
+app.get("/api/status",function(req,res){
+    //res.JSON() automatically serailizes the object and sets the JSON content type
+    res.status(201).json({
+        success:true,
+        messge:"Express API is working"
+    });
+});
+
+app.get("/api/error",function(req,res){
+    //res.JSON() automatically serailizes the object and sets the JSON content type
+    res.status(404).json({
+        success:false,
+        messge:"requested resourse is not found."
+    });
+});
+app.listen(4001,function(){
+    console.log("Express server running at http://localhost:4001")
+});
