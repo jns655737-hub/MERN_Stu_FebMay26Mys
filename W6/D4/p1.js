471d42718b16d187311709e4e793e98ca483bf8d
jns655737-hub <jns655737@gmail.com>
2026-04-01 11:42:27 +0530

    understanding the http module

---
+++ W6/D4/p1.js
@@ -0,0 +1,6 @@
+//understanding the http module
+//importing uildin http module(common js)
+const http=require("http");
+
+console.log("Type of http.create server:",typeof http.createServer);
+console.log("common http methods:",http.METHODS);
