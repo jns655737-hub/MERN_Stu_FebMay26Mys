832d72d3df425db7b455d6151d82d466232be5ff
jns655737-hub <jns655737@gmail.com>
2026-04-08 10:07:23 +0530

    User's details of the application

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/users.js
@@ -0,0 +1,19 @@
+//USer's details of the application
+const users=[
+    {
+        id:1,
+        name:"Jeevan",
+        enmail:"u@e.com",
+        password:"abc@123",
+        role:"user"
+    },
+    {
+        id:2,
+        name:"Admin",
+        enmail:"a@e.com",
+        password:"admin@123",
+        role:"admin"
+    }
+];
+
+module.exports=users;
