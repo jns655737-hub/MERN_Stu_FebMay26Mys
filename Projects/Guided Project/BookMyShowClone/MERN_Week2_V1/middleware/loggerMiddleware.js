8d0bdeec2c22513eec0dc0d5bbfadb04eda65a7c
jns655737-hub <jns655737@gmail.com>
2026-04-08 10:30:09 +0530

    Middleware for req log

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/loggerMiddleware.js
@@ -0,0 +1,8 @@
+//log is used to track when req come and from where req come
+//Middleware for req log
+function loggerMiddleware(req,res,next){
+    console.log(`${req.method} ${req.originalUrl}`);
+    next()
+}
+module.exports = loggerMiddleware;
+//Task:write the log to a file along with timestamp with both req & res
