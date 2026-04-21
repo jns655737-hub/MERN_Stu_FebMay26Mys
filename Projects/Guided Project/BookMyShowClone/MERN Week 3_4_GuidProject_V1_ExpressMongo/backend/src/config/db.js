7490b536583e2a232cd37e3109dbc0f4a73c1f2c
jns655737-hub <jns655737@gmail.com>
2026-04-21 10:21:21 +0530

    DB connection function added

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/config/db.js
@@ -0,0 +1,16 @@
+//Connection related function
+const mongoose=require("mongoose");
+
+const connectDB=async () => {
+    try{
+         const conn=await mongoose.connect(process.env.MONGO_URI);
+         console.log(`MongoDB connected:${conn.connection.host}`);
+         
+    }
+    catch(error){
+    console.error("DB Connection failed",error.message);
+
+    process.exit(1);
+    }
+}
+module.exports=connectDB;
