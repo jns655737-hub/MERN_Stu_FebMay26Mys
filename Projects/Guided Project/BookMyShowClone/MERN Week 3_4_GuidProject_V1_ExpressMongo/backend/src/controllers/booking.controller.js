607bf456fcc6dcfa8e216da11950f0c58523216e
jns655737-hub <jns655737@gmail.com>
2026-04-23 15:29:38 +0530

    controller for creation ,cancel & get booking is added

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/controllers/booking.controller.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/controllers/booking.controller.js
@@ -1,64 +1,49 @@
-const movieService=require("../services/movie.service");
+const bookingService=require("../services/booking.service")
 
-//create movie 
-exports.createMovie=async (req,res,next) => {
+///creat booking
+exports.createBooking =async (req,res,next) => {
     try{
-        const movie=await movieService.createMovie(req.body);
-
-        res.status(201).json({
+         const booking=await bookingService.createBooking(
+            req.user._id,
+            req.body
+         );
+         res.status(201).json({
             success:true,
-            message:"Movie created successfully",
-            data:movie,
-        });
+            message:"Booking confirmed",
+            data:booking,
+         });
     }
     catch(error){
         next(error)
     }
 };
 
-//Get Movies
-exports.getMovies=async (req,res,next) => {
+//get bookings
+exports.getMyBookings = async (req,res,next) => {
     try{
-        const result=await movieService.getMovies(req.query);
-
-        res.status(200).json({
+     const booking =await  bookingService.getUserBookings(req.user._id);
+     res.status(200).json({
             success:true,
-            message:"Movie List fetched",
-            data:result,
-        });
+            message:"Booking fetched",
+            data:bookings,
+         });
     }
     catch(error){
         next(error)
     }
-};
+}
 
-//update Movies
-exports.updateMovie=async (req,res,next) => {
+//cancel bookings
+exports.cancelBooking = async (req,res,next) => {
     try{
-        const movie=await movieService.updateMovie(req.params.id,req.body);
-
-        res.status(200).json({
+     await  bookingService.cancelBooking(req.user._id,req.params);
+     res.status(200).json({
             success:true,
-            message:"Movie updated successfully",
-            data:movie,
-        });
+            message:"Booking cancelled",
+            data:bookings,
+         });
     }
     catch(error){
         next(error)
     }
-};
-
-//deleteMovie Movies
-exports.deleteMovie=async (req,res,next) => {
-    try{
-        await movieService.deleteMovie(req.params.id);
-
-        res.status(200).json({
-            success:true,
-            message:"Movie deleted successfully",
-        });
-    }
-    catch(error){
-        next(error)
-    }
-};
+}
