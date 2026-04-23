0e2785407c7045192af78cb43d57c0c45a2a658b
jns655737-hub <jns655737@gmail.com>
2026-04-23 10:54:24 +0530

    Added show routes

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const authRoutes = require("./src/routes/auth.routes");
 const movieRoutes=require("./src/routes/movie.routes");
-const movieRoutes=require("./src/routes/movie.routes");
-const movieRoutes=require("./src/routes/booking.routes");
+const bookingRoutes=require("./src/routes/booking.routes");
+const showRoutes=require("./src/routes/show.routes");
 
 
 const app = express();
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use("/api/auth",authRoutes);
 app.use("/api/movies",movieRoutes)
 app.use("/api/bookings",bookingRoutes)
+app.use("/api/shows",showRoutes)
 
 
 //Base URL
