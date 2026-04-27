6446c3d4f591a7027b7074bb3b3898bba54f6f01
jns655737-hub <jns655737@gmail.com>
2026-04-27 15:09:27 +0530

    Created React app using Vite

---
+++ W9/D1/my-app1/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App(){
+           return(
+            <div>
+                <h1>React Introduction</h1>
+                <p>React Simplifies UI creation through reusable components</p>
+            </div>
+           );
+        }
+
+export default App
