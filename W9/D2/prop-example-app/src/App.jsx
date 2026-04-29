1638790952b333202d0605026531043243029d74
jns655737-hub <jns655737@gmail.com>
2026-04-29 13:50:26 +0530

    import PropsImmutability

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -3,7 +3,10 @@ import { useState } from 'react'
 // import{PropDestructuring} from './components/P2'
 // import{PropsChildren} from './components/P3'
 // import{PropDrillingDemo} from './components/P4'
-import{SharedStateParent} from './components/P5'
+// import{SharedStateParent} from './components/P5'
+// import{CompositionDemo} from './components/P6'
+// import{ObjectsAndArrayProps} from './components/P7'
+import{PropsImmutability} from './components/P8'
 
 
 import './App.css'
@@ -16,7 +19,10 @@ function App() {
     {/* <PropDestructuring/> */}
     {/* <PropsChildren/> */}
     {/* <PropDrillingDemo/> */}
-    <SharedStateParent/>
+    {/* <SharedStateParent/> */}
+    {/* <CompositionDemo/> */}
+    {/* <ObjectsAndArrayProps/> */}
+    <PropsImmutability/>
     </>
   )
 }
