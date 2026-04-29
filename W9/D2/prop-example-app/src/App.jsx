7a58d2171ab26b19fc287adf9f8890b017fa29d2
jns655737-hub <jns655737@gmail.com>
2026-04-29 11:44:38 +0530

    import SharedStateParent

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
-import {PropBasics} from './components/P1'
+// import {PropBasics} from './components/P1'
+// import{PropDestructuring} from './components/P2'
+// import{PropsChildren} from './components/P3'
+// import{PropDrillingDemo} from './components/P4'
+import{SharedStateParent} from './components/P5'
+
+
 import './App.css'
 
 function App() {
   
   return (
     <>
-    <PropBasics/>
+    {/* <PropBasics/> */}
+    {/* <PropDestructuring/> */}
+    {/* <PropsChildren/> */}
+    {/* <PropDrillingDemo/> */}
+    <SharedStateParent/>
     </>
   )
 }
