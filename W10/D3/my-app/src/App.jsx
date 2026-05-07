49ff3e0589cf9827f9deac12adea768ade7583b9
jns655737-hub <jns655737@gmail.com>
2026-05-07 11:42:02 +0530

    updated

--- W10/D3/my-app/src/App.jsx
+++ W10/D3/my-app/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import {EventBasics} from "./components/P1"
 import {EventObject} from "./components/P2"
+import {PassingArguements} from "./components/P3"
 
 function App() {
 
   return (
     <>
       {/* <EventBasics/> */}
-      <EventObject/>
+      {/* <EventObject/> */}
+      <PassingArguements/>
     </>
   )
 }
