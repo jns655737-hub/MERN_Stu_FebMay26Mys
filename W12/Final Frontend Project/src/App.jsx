a875fd1821e7bfd61971189f99d202616ebc2b51
jns655737-hub <jns655737@gmail.com>
2026-06-12 11:36:35 +0530

    single  Component Composition,parent-child components

---
+++ W12/Final Frontend Project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+// import MovieCard from "./components/MovieCard";
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    //Component composition
+    //when we create multiple page and wrap in single page 
+    <HomePage/>
+  );
+}
+
+export default App;
---
+++ W12/Final Frontend Project/src/components/HeroBanner.jsx
@@ -0,0 +1,8 @@
+export default function HeroBanner(){
+    return(
+        <section className="hero">
+           <h4>Welcome to BookMyShow Application</h4>
+           <p>India's most loved movie booking app</p>
+        </section>
+    )
+}
